c7398c813af67f3d85ec7dccf8a91d76ed1f1edd
duboviy <eugene.dubovoy@gmail.com>
2017-04-17 22:58:51 +0300

    Create util.h

---
+++ c_macros_util/util.h
@@ -0,0 +1,29 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+/* Types */
+
+typedef char       int8;
+typedef short      int16;
+typedef int        int32;
+typedef long long  int64;
+
+typedef unsigned char       uint8;
+typedef unsigned short      uint16;
+typedef unsigned int        uint32;
+typedef unsigned long long  uint64;
+
+/* Macros */
+
+#define STR_EMPTY(s)           ((s[0]) == '\0')
+#define STR_EQUAL(a, b)        (strcmp((a), (b)) == 0)
+#define STR_START_WITH(a, b)   (strncmp((a), (b), strlen((b))) == 0)
+#define GET_LEN_INT(size)      ((size) / sizeof(int))
+
+#undef MAX
+#undef MIN
+
+#define MAX(a, b)              ((a) > (b) ? (a) : (b))
+#define MIN(a, b)              ((a) < (b) ? (a) : (b))
+
+#endif
